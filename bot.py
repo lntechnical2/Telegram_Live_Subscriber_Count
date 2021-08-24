058e9587e7c91327257a1b8d737579cda7fe6328
lokaman chendekar <82169706+lntechnical2@users.noreply.github.com>
2021-08-24 10:18:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters
 import time 
 ADMIN = os.environ.get("ADMIN", "")
 TOKEN = os.environ.get("TOKEN", "")
-APP_ID = int(os.environ.get("API_ID",12345))
+API_ID = int(os.environ.get("API_ID",12345))
 API_HASH = os.environ.get("API_HASH", "")
 
 app = Client("livebot" ,bot_token = TOKEN ,api_id = API_ID ,api_hash = API_HASH )
