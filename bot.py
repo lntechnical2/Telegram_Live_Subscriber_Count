c49932ee90cdf93a680ee1be2752673ed4b6e4fa
lokaman chendekar <82169706+lntechnical2@users.noreply.github.com>
2021-08-26 15:19:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,18 +20,14 @@ x = []
 @app.on_message(filters.user(ADMIN) & filters.command("livecount",prefixes='!'))
 async def livecount(client,message):
   count1 = await app.get_chat_members_count(CH_USER_NAME)
+  x.append(count1)
   ms = await message.reply_text(f"Live Telegram Subscriber Counting\n {CH_USER_NAME} - {count1}")
   while True :
    time.sleep(3)
-   count = await app.get_chat_members_count(CH_USER_NAME)   
-   try:
-      if x[0] == count:
-       time.sleep(3)
-      else:
+   count = await app.get_chat_members_count(CH_USER_NAME)  
+   if x[0] != count:
        del x[0]
        x.append(count)
        await ms.edit(f"Live Telegram Subscriber Counting\n {CH_USER_NAME} - {x[0]}")
-   except :
-    x.append(count)
  
 app.run()
